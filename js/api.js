54addcee1270edaa06d830557f9fea69ccc653d5
Ivan Melnykov <i.melnykow2023@gmail.com>
2026-09-15 10:46:28 +0300

    Initial API fetch logic completed, no styles applied yet

--- js/api.js
+++ js/api.js
@@ -1,1 +1,19 @@
 const API_KEY = '778b3059319413934e250574f3d9c2c4';
+
+export async function fetchWeather(city) {
+    try {
+        const response = await fetch (
+           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}` 
+        );
+
+        if(!response.ok) {
+            throw new Error(`City not found: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error("Failed to fetch weather data:", error);
+        return null;
+      }
+}
--- js/app.js
+++ js/app.js
@@ -1,7 +1,29 @@
-const searchbutton = document.querySelector("search-button");
-const searchinput = document.querySelector("input");
-searchbutton.addEventListener("click", () => {
-  const city = searchinput.value;
-  console.log("Searching for: " ,city);
+import { fetchWeather } from "./api.js";
 
+
+const searchbutton = document.querySelector(".search-button");
+const searchinput = document.querySelector("input");
+const weatherInfoDiv = document.querySelector(".weather-info");
+
+searchbutton.addEventListener("click", async () => {
+  const city = searchinput.value.trim();
+
+  if(!city) return; 
+
+  const data = await fetchWeather(city);
+
+  if(data) {
+
+    const temp = Math.round(data.main.temp);
+    const cityName = data.name;
+
+    weatherInfoDiv.innerHTML = `
+     <h3>${cityName}</h3>
+     <p>${temp}°C</p>
+    `;
+    } else {
+      weatherInfoDiv.innerHTML = `<p> Could not find weather for "${city}".</p>`;
+
+    }
+  
 });
