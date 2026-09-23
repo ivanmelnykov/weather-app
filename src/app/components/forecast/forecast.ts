ebb06c4555b5212a87306605c7d4359466d8f885
Ivan Melnykov <i.melnykow2023@gmail.com>
2026-09-23 16:56:40 +0300

    Completed the configuration of files and folder structure

---
+++ src/app/components/forecast/forecast.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-forecast',
+  styleUrl: './forecast.scss',
+  templateUrl: './forecast.html',
+})
+export class Forecast {}
---
+++ src/app/components/weather-card/weather-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-weather-card',
+  styleUrl: './weather-card.scss',
+  templateUrl: './weather-card.html',
+})
+export class WeatherCard {}
